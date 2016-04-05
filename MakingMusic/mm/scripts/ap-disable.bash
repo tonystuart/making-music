7702f204a47c3e1e8981a3ffe6fc778f20ac6243
Tony Stuart <anthonyfstuart@gmail.com>
2016-04-05 06:29:48 -0700

    Add support for starting WebApp at system boot

--- MakingMusic/mm/scripts/ap-disable.bash
+++ MakingMusic/mm/scripts/ap-disable.bash
@@ -7,6 +7,7 @@ sudo cp -v /home/pi/mm/config/etc/dnsmasq.conf.0 /etc/dnsmasq.conf
 sudo cp -v /home/pi/mm/config/etc/default/hostapd.0 /etc/default/hostapd
 sudo cp -v /home/pi/mm/config/etc/network/interfaces.0 /etc/network/interfaces
 sudo cp -v /home/pi/mm/config/etc/hostapd/hostapd.conf.0 /etc/hostapd/hostapd.conf
+sudo cp -v /home/pi/mm/config/etc/rc.local.0 /etc/rc.local
 
 sudo update-rc.d hostapd disable
 sudo update-rc.d dnsmasq disable
--- MakingMusic/mm/scripts/ap-enable.bash
+++ MakingMusic/mm/scripts/ap-enable.bash
@@ -4,6 +4,7 @@ sudo cp -v /home/pi/mm/config/etc/dnsmasq.conf.1 /etc/dnsmasq.conf
 sudo cp -v /home/pi/mm/config/etc/default/hostapd.1 /etc/default/hostapd
 sudo cp -v /home/pi/mm/config/etc/network/interfaces.1 /etc/network/interfaces
 sudo cp -v /home/pi/mm/config/etc/hostapd/hostapd.conf.1 /etc/hostapd/hostapd.conf
+sudo cp -v /home/pi/mm/config/etc/rc.local.1 /etc/rc.local
 
 sudo update-rc.d hostapd enable
 sudo update-rc.d dnsmasq enable
